3cc149ecbb065317c0338bbf06aa435f69c3f93c
Paranietharan074 <paranietharan64@gmail.com>
2024-02-16 22:41:35 +0530

    .module.css work completed

--- src/Components/ArticleSummary.js
+++ src/Components/ArticleSummary.js
@@ -1,18 +1,19 @@
 import React from 'react';
-import './ArticleSummaryStyle.css';
+import styles from './ArticleSummaryStyle.module.css'; // Import the CSS module
+// ^ Note the change in the import statement
 
 function ArticleSummary(props) {
   return (
-    <div className="article-summary-wrapper">
-      <div className="article-summary-container">
-        <img className="article-image" src={props.articleImage} alt="Article" />
-        <p className="article-summary">{props.summary}</p>
-        <h2 className="article-title">{props.title}</h2>
-        <div className='author-info'>
-          <img className="author-image" src={props.authorImage} alt="Author" />
-          <p className="author-name">{props.authorName}</p>
+    <div className={styles['article-summary-wrapper']}> {/* Use the scoped class name */}
+      <div className={styles['article-summary-container']}> {/* Use the scoped class name */}
+        <img className={styles['article-image']} src={props.articleImage} alt="Article" /> {/* Use the scoped class name */}
+        <p className={styles['article-summary']}>{props.summary}</p> {/* Use the scoped class name */}
+        <h2 className={styles['article-title']}>{props.title}</h2> {/* Use the scoped class name */}
+        <div className={styles['author-info']}>
+          <img className={styles['author-image']} src={props.authorImage} alt="Author" /> {/* Use the scoped class name */}
+          <p className={styles['author-name']}>{props.authorName}</p> {/* Use the scoped class name */}
         </div>
-        <p className="publication-date">Published {props.publishedAgo}</p>
+        <p className={styles['publication-date']}>Published {props.publishedAgo}</p> {/* Use the scoped class name */}
       </div>
     </div>
   );
--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import SearchIcon from '@mui/icons-material/Search';
-import './SearchBar.css';
+import styles from './SearchBar.module.css'; // Import CSS module file
 
 function SearchBar(props) {
     const [searchText, setSearchText] = useState('');
@@ -15,24 +15,27 @@ function SearchBar(props) {
     };
 
     const handleKeyDown = (event) => {
-        // Trigger search if Enter key is pressed
+        // Trigger search if the Enter key is pressed
         if (event.key === 'Enter') {
             handleSearch();
         }
     };
 
     return (
-        <div className="search-bar">
-            <input
-                type="text"
-                placeholder="Search..."
-                value={searchText}
-                onChange={handleInputChange}
-                onKeyDown={handleKeyDown}
-            />
-            <button onClick={handleSearch}>
-                <SearchIcon />
-            </button>
+        <div className={styles.searchBar}>
+            <div className={styles.inputContainer}>
+                <input
+                    type="text"
+                    placeholder="Search..."
+                    value={searchText}
+                    onChange={handleInputChange}
+                    onKeyDown={handleKeyDown}
+                    className={styles.input}
+                />
+                <button onClick={handleSearch} className={styles.button}>
+                    <SearchIcon />
+                </button>
+            </div>
         </div>
     );
 }

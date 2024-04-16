cd18e42892f148f353780dd2bbfbd4f97c6947ce
paranietharan74 <paranietharan64@gmail.com>
2024-04-16 23:51:19 +0530

    Change

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,20 +12,8 @@ const footerStyle = {
     backgroundColor: '#f4f4f4',
     color: '#333',
     textAlign: 'center',
-    padding: '20px',
+    padding: '0.2rem',
     marginTop: 'auto',
-    height: '5vh',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderRadius: '2px',
-    borderColor: '#666666',
-    borderStyle: 'solid',
-    borderWidth: '1.5px',
-    fontWeight: 'bold',
-    marginBottom: '5px',
-    marginLeft: '5px',
-    marginRight: '5px',
-};
+}
 
 export default Footer;
